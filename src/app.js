8f4b061fdd9a9d90faff61093fd8e002422bd366
Ketan <contactketan080@gmail.com>
2026-09-22 18:24:58 +0530

    server test dummy api

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+
+app.get('/',(req,res)=>{
+  res.send("Ledger Service is Up and Running")
+})
 app.use('/api/auth',authRoutes);
 app.use('/api/accounts', accountRouter)
 app.use('/api/transactions',transactionRoutes)
